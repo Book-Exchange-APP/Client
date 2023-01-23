ad49a70e3238f4aab733c90385272c6f2d2ba6c6
Hanzhi11 <hanzhizhang11@gmail.com>
2023-01-23 15:32:55 +1000

    completed header/navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,24 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import '../styles/Navbar.css'
 
 const Navbar = () => {
-    
+    return (
+        <>
+            <nav class="navbar navbar-expand fixed-top bg-gradient">
+                <div id='logo'>
+                    <a class="nav-link" href="/">
+                        <img src="../logo.png" alt="Logo" width="80" />
+                    </a>
+                </div>
+                <div id="navbarNav" class="nav justify-content-end container-fluid">
+                    <a class="nav-link text-$green-700" href="/books">All Books</a>
+                    <a class="nav-link" href="/login">Admin Login</a>
+                    <a class="nav-link" href="/contact">Contact</a>
+                </div>
+            </nav>
+        </>
+    )
 }
 
 export default Navbar
