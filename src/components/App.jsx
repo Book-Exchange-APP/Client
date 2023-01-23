d8c3d8b0a248aa90d2144e162b05a91b11da97af
Hanzhi11 <hanzhizhang11@gmail.com>
2023-01-23 12:03:16 +1000

    set up routes

---
+++ src/components/App.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import Navbar from './Navbar'
+import Home from './Home'
+import Books from './Books'
+import Confirmation from './Confirmation'
+import Contact from './Contact'
+import Login from './Login'
+import Dashboard from './Dashboard'
+import Footer from './Footer'
+import { Routes, Route } from 'react-router-dom'
+
+
+const App = () =>{
+  const ShowBookWrapper = () => {
+
+  }
+
+return (
+    <>
+    <Navbar />
+    <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/books' element={<Books />} />
+        <Route path='/book/:id' element={<ShowBookWrapper />} />
+        <Route path='/appointment/:id/confirmation' element={<Confirmation />} />
+        <Route path='/contact' element={<Contact />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/dashboard' element={<Dashboard />} />
+        <Route path='*' element={<h4>Page not found!</h4>} />
+      </Routes>
+    <Footer />
+    </>
+)
+}
+
+export default App
