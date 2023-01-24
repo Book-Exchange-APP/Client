c1b32d3d954be6268bc9efa6b463e3bccb42d0f4
fishball0741 <fishball0741@gmail.com>
2023-01-24 18:54:09 +1000

    home added

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,21 +1,55 @@
 import React from 'react'
-// import { Link } from "react-router-dom"
+import '../styles/Home.css'
 
 
 const Home = () => {
     return (
         <>
-            <h1>HOME PAGE</h1>
-            <h1>HOME PAGE</h1>
-            <h1>HOME PAGE</h1>
-            <h1>HOME PAGE</h1>
-            <h1>HOME PAGE</h1>
-            <h1>HOME PAGE</h1>
-            <h1>HOME PAGE</h1>
-            <h1>HOME PAGE</h1>
-            <h1>HOME PAGE</h1>
-            <h1>HOME PAGE</h1>
-            
+            <div className="Home">
+                <div className="title text-center">
+                    <h1>Title Banner</h1>
+                </div>
+                <div className="Search">
+                    <h1>Search Box</h1>
+                </div><div className="Locat">
+                    <h1>Our Location</h1>
+                </div>
+
+                    <h2 className='text-center text info'>Featured Books</h2>
+                    <div className='books'>
+                        <img src="../src/assets/logo.png" alt="book" />
+                        <div className='text'>
+                            <span>Book Details</span>
+                        </div>
+                    </div>
+                    <div className='books'>
+                        <img src="../src/assets/logo.png" alt="book" />
+                        <div className='text'>
+                            <span>Book Details</span>
+                        </div>
+                    </div>
+                    <div className='books'>
+                        <img src="../src/assets/logo.png" alt="book" />
+                        <div className='text'>
+                            <span>Book Details</span>
+                        </div>
+                    </div>
+                    <div className='books'>
+                        <img src="../src/assets/logo.png" alt="book" />
+                        <div className='text'>
+                            <span>Book Details</span>
+                        </div>
+                    </div>
+                    <div className='container-fluid mx-2'>
+                    <div className='row mt-5 mx-2'>
+                        <div className='col-md-3'>
+
+                        </div>
+                    </div>
+                    </div>
+                    <h2 className='text-center text info'>Latest Arrivals</h2>
+            </div>
+
         </>
     )
 }
