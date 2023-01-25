7c03b443c8de94d8e3d6aab5807f3f0c715d05c2
fishball0741 <fishball0741@gmail.com>
2023-01-25 15:15:39 +1000

    Home done 1

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,75 +1,108 @@
-import React from 'react'
 import '../styles/Home.css'
+import React, { useState } from 'react'
+import ShowBooks from './ShowBooks'
+import Card from 'react-bootstrap/Card';
 
 
-const Home = () => {
+
+
+const Home = ({ books, locations, languages, conditions, genres, searchBook }) => {
+    const [author, setAuthor] = useState('')
+    const [title, setTitle] = useState('')
+    const [location, setLocation] = useState('')
+    const [language, setLanguage] = useState('')
+    const [condition, setCondition] = useState('')
+    const [genre, setGenre] = useState('')
+
+
+    function submit(evt) {
+        evt.preventDefault()
+        const searchCriteria = {}
+        if (author.length > 0) {
+            searchCriteria.author = author
+        }
+        if (title.length > 0) {
+            searchCriteria.title = title
+        }
+        if (location.length > 0 && location !== 'Location') {
+            searchCriteria.location = location
+        }
+        if (language.length > 0 && language !== 'Language') {
+            searchCriteria.language = language
+        }
+        if (condition.length > 0 && condition !== 'Condition') {
+            searchCriteria.condition = condition
+        }
+        if (genre.length > 0 && genre !== 'Genre') {
+            searchCriteria.genre = genre
+        }
+        searchBook(searchCriteria)
+    }
+
+
+
     return (
         <>
-            <div className="Home">
-                <div className="title text-center">
-                    <h1>Title Banner</h1>
-                </div>
-                <div className="SearchNav">
-                    <h2>Search Box</h2>
-                </div><div className="LocatNav">
-                    <h2>Our Location</h2>
-                </div>
+            <main>
+                <div className="Home">
+                    <div className="banner text-center">
+                        <h1>How To Use</h1>
+                    </div>
+                    <form className="p-3" role="search" onSubmit={submit}>
+                        <input className="title form-control mb-2" type="search" placeholder="Title" aria-label="Search" value={title} onChange={(evt) => setTitle(evt.target.value)}/>
+                        <input className="author form-control mb-2" type="search" placeholder="Author" aria-label="Search" value={author} onChange={(evt) => setAuthor(evt.target.value)} />
+                        <select className="location form-select mb-2" id="inputGroupSelect01" value={location} onChange={(evt) => setLocation(evt.target.value)}>
+                            <option>Location</option>
+                            {locations.map((location, index) => (
+                                <option key={index} value={location.location}>{location.location}</option>
+                            ))}
+                        </select>
+                        <select className="condition form-select mb-2" id="inputGroupSelect01" value={condition} onChange={(evt) => setCondition(evt.target.value)}>
+                            <option>Condition</option>
+                            {conditions.map((condition, index) => (
+                                <option key={index} value={condition}>{condition}</option>
+                            ))}
+                        </select>
+                        <select className="language form-select mb-2" id="inputGroupSelect01" value={language} onChange={(evt) => setLanguage(evt.target.value)}>
+                            <option>Language</option>
+                            {languages.map((language, index) => (
+                                <option key={index} value={language}>{language}</option>
+                            ))}
+                        </select>
+                        <select className="genre form-select mb-2" id="inputGroupSelect01" value={genre} onChange={(evt) => setGenre(evt.target.value)}>
+                            <option>Genre</option>
+                            {genres.map((genre, index) => (
+                                <option key={index} value={genre}>{genre}</option>
+                            ))}
+                        </select>
+                        <button className="search btn btn-outline-success fs-6" type="submit">Search</button>
+                    </form>
+
+                    <div>
+                        <div id="address" className="nav">
+                        
+                            <a className="loc-link" href="https://www.google.com/maps" target="_blank">1 Main St</a>   
+                            <a className="loc-link" href="https://www.google.com/maps" target="_blank">12 Main St</a>
+                            <a className="loc-link" href="https://www.google.com/maps" target="_blank">123 Main St</a>
+                        </div>
+                        
+                    </div>
                     <div className="feature text-center">
-                        <h2>Featured Books</h2>
-                    </div>
-                    <div className='books'>
-                        <img src="../src/assets/logo.png" alt="book" />
-                        <div className='text'>
-                            <span>Book Details</span>
+                        <h1 className='Fbooks'>Featured Books</h1>
+                        <div className='fourbooks'>
+                            {books.length>0 ? <ShowBooks books={books}/> : <h1 className='text-center pt-5 text-danger'>No Books Found!</h1>}
                         </div>
                     </div>
-                    <div className='books'>
-                        <img src="../src/assets/logo.png" alt="book" />
-                        <div className='text'>
-                            <span>Book Details</span>
-                        </div>
-                    </div>
-                    <div className='books'>
-                        <img src="../src/assets/logo.png" alt="book" />
-                        <div className='text'>
-                            <span>Book Details</span>
-                        </div>
-                    </div>
-                    <div className='books'>
-                        <img src="../src/assets/logo.png" alt="book" />
-                        <div className='text'>
-                            <span>Book Details</span>
-                        </div>
-                    </div>
-                    <br></br>
                     <div className="latest text-center">
-                        <h2>Latest Books</h2>
-                    </div>
-                    <div className='books'>
-                        <img src="../src/assets/logo.png" alt="book" />
-                        <div className='text'>
-                            <span>Book Details</span>
+                        <h1 className='Lbooks'>Latest Book</h1>
+                        <div className='fourbooks'>
+                            {books.length>0 ? <ShowBooks books={books}/> : <h1 className='text-center pt-5 text-danger'>No Books Found!</h1>}
                         </div>
                     </div>
-                    <div className='books'>
-                        <img src="../src/assets/logo.png" alt="book" />
-                        <div className='text'>
-                            <span>Book Details</span>
-                        </div>
-                    </div>
-                    <div className='books'>
-                        <img src="../src/assets/logo.png" alt="book" />
-                        <div className='text'>
-                            <span>Book Details</span>
-                        </div>
-                    </div>
-                    <div className='books'>
-                        <img src="../src/assets/logo.png" alt="book" />
-                        <div className='text'>
-                            <span>Book Details</span>
-                        </div>
-                    </div>
-            </div>
+                        
+                    
+                </div>
+            </main>
 
         </>
     )
