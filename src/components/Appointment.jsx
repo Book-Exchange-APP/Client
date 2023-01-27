e17c3de64232475edbe7af42e04b484ebdf46fa0
fishball0741 <fishball0741@gmail.com>
2023-01-27 16:33:32 +1000

    home 'see more' added

--- src/components/Appointment.jsx
+++ src/components/Appointment.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import ShowBook from './ShowBook'
+
 
 const Appointment = () => {
   return (
-    <div>Appointment</div>
+    <div>
+        <div className='title'>
+            <h1>Appointment Details</h1>
+        </div>
+        <ShowBook book />
+    </div>
+    
+
+
+
+
   )
 }
 
