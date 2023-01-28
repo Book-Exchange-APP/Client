2c4c04ddebf57e2f40420d626d5392ed2479bb78
Hanzhi11 <hanzhizhang11@gmail.com>
2023-01-28 20:14:18 +1000

    corrected id name and removed unused id and class names

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -45,17 +45,17 @@ const Search = ({ locations, languages, conditions, genres, searchBook }) => {
     }
 
     return (
-        <form id="search-form" className="p-3" role="search" onSubmit={(evt) => submit(evt)} value={1}>
-            <input id="input-title" name="title" className="form-control mb-2" type="search" placeholder="Title" aria-label="Search" value={title} onChange={(evt) => setTitle(evt.target.value)} />
-            <input id="input-author" name="author" className="form-control mb-2" type="search" placeholder="Author" aria-label="Search" value={author} onChange={(evt) => setAuthor(evt.target.value)} />
+        <form id="searchForm" className="p-3" role="search" onSubmit={(evt) => submit(evt)} value={1}>
+            <input id="inputTitle" name="title" className="form-control mb-2" type="search" placeholder="Title" aria-label="Search" value={title} onChange={(evt) => setTitle(evt.target.value)} />
+            <input id="inputAuthor" name="author" className="form-control mb-2" type="search" placeholder="Author" aria-label="Search" value={author} onChange={(evt) => setAuthor(evt.target.value)} />
             <div className="options">
-                <select id="select-location" name="location" className="form-select mb-2" value={location} onChange={(evt) => setLocation(evt.target.value)}>
+                <select id="selectLocation" name="location" className="form-select mb-2" value={location} onChange={(evt) => setLocation(evt.target.value)}>
                     <option>Location</option>
                     {locations.map((location, index) => (
                         <option key={index} value={location.location}>{location.location}</option>
                     ))}
                 </select>
-                <select id="select-condition" name="condition" className="form-select mb-2" value={condition} onChange={(evt) => setCondition(evt.target.value)}>
+                <select id="selectCondition" name="condition" className="form-select mb-2" value={condition} onChange={(evt) => setCondition(evt.target.value)}>
                     <option selected>Condition</option>
                     {conditions.map((condition, index) => (
                         <option key={index} value={condition.name}>{condition.name}</option>
@@ -63,13 +63,13 @@ const Search = ({ locations, languages, conditions, genres, searchBook }) => {
                 </select>
             </div>
             <div className="options">
-                <select id="select-language" name="language" className="form-select mb-2" value={language} onChange={(evt) => setLanguage(evt.target.value)}>
+                <select id="selectLanguage" name="language" className="form-select mb-2" value={language} onChange={(evt) => setLanguage(evt.target.value)}>
                     <option selected>Language</option>
                     {languages.map((language, index) => (
                         <option key={index} value={language.name}>{language.name}</option>
                     ))}
                 </select>
-                <select id="select-genre" name="genre" className="form-select mb-2" value={genre} onChange={(evt) => setGenre(evt.target.value)}>
+                <select id="selectGenre" name="genre" className="form-select mb-2" value={genre} onChange={(evt) => setGenre(evt.target.value)}>
                     <option selected>Genre</option>
                     {genres.map((genre, index) => (
                         <option key={index} value={genre.name}>{genre.name}</option>
