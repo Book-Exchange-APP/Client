7e753affb47528755c0414a62204c53dfcc2e891
Hanzhi11 <hanzhizhang11@gmail.com>
2023-01-29 16:33:00 +1000

    completed test on search

--- src/tests/Search.test.jsx
+++ src/tests/Search.test.jsx
@@ -1,21 +1,23 @@
 import '@testing-library/jest-dom'
 import { render, screen } from "@testing-library/react"
 import { BrowserRouter } from 'react-router-dom'
-import Books from "../components/Books"
+import Search from "../components/Search"
 import { displayedBooks, locations, languages, conditions, genres } from "../mocks/handlers"
 import { AuthContextProvider } from '../auth/AuthContext'
+import { MemoryRouter } from 'react-router-dom'
 
 
-describe('Books Component', () => {
+describe('Search Component', () => {
     let h1
     let h2
     let h5
+    let p
     let bookImg
     let detailsBtn
     let searchBar
 
     beforeEach(function () {
-        render(<BrowserRouter><AuthContextProvider><Books books={displayedBooks} locations={locations} languages={languages} conditions={conditions} genres={genres}/></AuthContextProvider></BrowserRouter>)
+        render(<MemoryRouter initialEntries={["?location=&language=&condition=Good&genre="]}><AuthContextProvider><Search books={displayedBooks} locations={locations} languages={languages} conditions={conditions} genres={genres}/></AuthContextProvider></MemoryRouter>)
         h1 = screen.getAllByRole('heading', { level: 1 })
         h2 = screen.queryAllByRole('heading', { level: 2 })
         h5 = screen.getAllByRole('heading', { level: 5 })
@@ -27,14 +29,15 @@ describe('Books Component', () => {
     it("Shows the Books heading", () => {
         expect(h1).toHaveLength(1)
         expect(h1[0]).toHaveTextContent('Books')
-
+        expect(screen.queryAllByText('Searched for \'Good\'')).toBeTruthy()
     })
 
-    it("Shows the Book Cards", () => {
+    it("Shows one Book Card", () => {
         expect(h2).toHaveLength(0)
-        expect(h5).toHaveLength(3)
-        expect(bookImg).toHaveLength(3)
-        expect(detailsBtn).toHaveLength(3)
+        expect(h5).toHaveLength(1)
+        expect(h5[0]).toHaveTextContent("Test 1")
+        expect(bookImg).toHaveLength(1)
+        expect(detailsBtn).toHaveLength(1)
     })
 
     it("Shows the Search Bar", () => {
