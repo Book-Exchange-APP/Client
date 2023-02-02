30c78b49bde7e6c46173f8fc106928666673d3e8
code-ry <rybussey@gmail.com>
2023-02-02 11:43:37 +1000

    Dashboard config amended

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -9,6 +9,7 @@ import Login from './Login'
 import Dashboard from './Dashboard'
 import Footer from './Footer'
 import ShowBook from './ShowBook'
+import Unauthorised from './Unauthorised'
 // import Appointment from './Appointment'
 import Search from './Search'
 import { useAuthContext } from '../auth/useAuthContext'
@@ -314,24 +315,24 @@ const App = () => {
 
   const denyBooks = async (appointment) => {
 
-    let inc_book = books.find(book => book._id === appointment.inc_book._id)
-    let out_book = books.find(book => book._id === appointment.out_book._id)
-    let thisAppointment = appointments.find(appointment => appointment._id === appointment._id)
+    // let inc_book = books.find(book => book._id === appointment.inc_book._id)
+    // let out_book = books.find(book => book._id === appointment.out_book._id)
+    // let thisAppointment = appointments.find(appointment => appointment._id === appointment._id)
     // const user = JSON.parse(sessionStorage.getItem('user'))
 
     const updatedIncBook = {
-      title: inc_book.title,
-      author: inc_book.author,
-      condition: inc_book.condition,
-      location: inc_book.location,
-      language: inc_book.language,
-      img: inc_book.img,
-      genre: inc_book.genre,
-      description: inc_book.description,
+      title: appointment.inc_book.title,
+      author: appointment.inc_book.author,
+      condition: appointment.inc_book.condition,
+      location: appointment.inc_book.location,
+      language: appointment.inc_book.language,
+      img: appointment.inc_book.img,
+      genre: appointment.inc_book.genre,
+      description: appointment.inc_book.description,
       status: bookStatus[1]._id
     }
 
-    const returnedIncBook = await fetch(`http://localhost:4001/books/${inc_book._id}`, {
+    const returnedIncBook = await fetch(`http://localhost:4001/books/${appointment.inc_book._id}`, {
       method: "PUT",
       headers: {
         Accept: "application/json",
@@ -342,18 +343,18 @@ const App = () => {
 
 
     const updatedOutBook = {
-      title: out_book.title,
-      author: out_book.author,
-      condition: out_book.condition,
-      location: out_book.location,
-      language: out_book.language,
-      img: out_book.img,
-      genre: out_book.genre,
-      description: out_book.description,
+      title: appointment.out_book.title,
+      author: appointment.out_book.author,
+      condition: appointment.out_book.condition,
+      location: appointment.out_book.location,
+      language: appointment.out_book.language,
+      img: appointment.out_book.img,
+      genre: appointment.out_book.genre,
+      description: appointment.out_book.description,
       status: bookStatus[0]._id
     }
 
-    const returnedOutBook = await fetch(`http://localhost:4001/books/${out_book._id}`, {
+    const returnedOutBook = await fetch(`http://localhost:4001/books/${appointment.out_book._id}`, {
       method: "PUT",
       headers: {
         Accept: "application/json",
@@ -364,14 +365,14 @@ const App = () => {
 
 
     const updatedAppointment = {
-      first_name: thisAppointment.first_name,
-      last_name: thisAppointment.last_name,
-      inc_book: thisAppointment.inc_book,
-      out_book: thisAppointment.out_book,
-      time: thisAppointment.time,
-      date: thisAppointment.date,
+      first_name: appointment.first_name,
+      last_name: appointment.last_name,
+      inc_book: appointment.inc_book,
+      out_book: appointment.out_book,
+      time: appointment.time,
+      date: appointment.date,
       status: appointmentStatus[2],
-      location: thisAppointment.location
+      location: appointment.location
     }
 
     const returnedAppointment = await fetch(`http://localhost:4001/appointments/${appointment._id}`, {
@@ -412,7 +413,7 @@ const App = () => {
           <Route path='/contact' element={<Contact locations={locations} />} />
           <Route path='/register' element={<Register />} />
           <Route path='/login' element={<Login nav={nav} />} />
-          <Route path='/dashboard' element={<Dashboard appointments={appointments} appointmentStatus={appointmentStatus} denyBooks={denyBooks} swapBooks={swapBooks} pendingAppointments={pendingAppointments} updateBooks={updateBooks} updateAppointments={updateAppointments} bookStatus={bookStatus}/> } />
+          <Route path='/dashboard' element={ user && user.admin ? <Dashboard denyBooks={denyBooks} swapBooks={swapBooks} pendingAppointments={pendingAppointments} /> : <Unauthorised /> } />
           <Route path='*' element={<main><h1 className="my-5 text-center">Page not found!</h1></main>} />
         </Routes> :
         <main>
---
+++ src/components/Unauthorised.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Unauthorised = () => {
+  return (
+    <main id="main">
+       <h1 style={{color: "red"}}> Unauthorised Access </h1>
+    </main>
+  )
+}
+
+export default Unauthorised
