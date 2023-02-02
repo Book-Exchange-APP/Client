69c77f3d3b79664a686eaa58e7cc0e5dccf1ae1c
code-ry <rybussey@gmail.com>
2023-02-03 09:32:41 +1000

    error handling for Swap/denyBooks

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -11,6 +11,7 @@ import Footer from './Footer'
 import ShowBook from './ShowBook'
 import Unauthorised from './Unauthorised'
 // import Appointment from './Appointment'
+import { useLogout } from '../auth/useLogout'
 import Search from './Search'
 import { useAuthContext } from '../auth/useAuthContext'
 import { Routes, Route, useNavigate, useParams, Navigate } from 'react-router-dom'
@@ -34,6 +35,7 @@ const App = () => {
   const nav = useNavigate()
   const { user } = useAuthContext()
   const [error, setError] = useState({ error: false })
+  const { logout } = useLogout()
 
   useEffect(() => {
     async function fetchBooks() {
@@ -241,10 +243,6 @@ const App = () => {
 
   const swapBooks = async (appointment) => {
 
-    let inc_book = books.find(book => book.book._id === appointment.inc_book._id)
-    let out_book = books.find(book => book.book._id === appointment.out_book._id)
-    let thisAppointment = appointments.find(appointment => appointment._id === appointment._id)
-    // const user = JSON.parse(sessionStorage.getItem('user'))
     console.log(appointment)
     const updatedIncBook = {
       title: appointment.inc_book.title,
@@ -259,67 +257,71 @@ const App = () => {
     }
 
     if (user) {
-      const returnedIncBook = await fetch(`http://localhost:4001/books/${appointment.inc_book._id}`, {
-        method: "PUT",
-        headers: {
-          Accept: "application/json",
-          "Content-Type": "application/json", 'Authorization': `Bearer ${user.token}`
-        },
-        body: JSON.stringify(updatedIncBook)
-      })}
+        const returnedIncBook = await fetch(`http://localhost:4001/books/${appointment.inc_book._id}`, {
+          method: "PUT",
+          headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json", 'Authorization': `Bearer ${user.token}`
+          },
+          body: JSON.stringify(updatedIncBook)
+        })
 
-    const updatedOutBook = {
-      title: appointment.out_book.title,
-      author: appointment.out_book.author,
-      condition: appointment.out_book.condition,
-      location: appointment.out_book.location,
-      language: appointment.out_book.language,
-      img: appointment.out_book.img,
-      genre: appointment.out_book.genre,
-      description: appointment.out_book.description,
-      status: bookStatus[1]._id
+        if (returnedIncBook.status === 401 ){
+          console.log('Error: Token Expired')
+          logout()
+          nav('/login')
+
+          return
+        }
+        
+
+        const updatedOutBook = {
+          title: appointment.out_book.title,
+          author: appointment.out_book.author,
+          condition: appointment.out_book.condition,
+          location: appointment.out_book.location,
+          language: appointment.out_book.language,
+          img: appointment.out_book.img,
+          genre: appointment.out_book.genre,
+          description: appointment.out_book.description,
+          status: bookStatus[1]._id
+        }
+
+        const returnedOutBook = await fetch(`http://localhost:4001/books/${appointment.out_book._id}`, {
+          method: "PUT",
+          headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json", 'Authorization': `Bearer ${user.token}`,
+          },
+          body: JSON.stringify(updatedOutBook)
+        })
+
+        const updatedAppointment = {
+          first_name: appointment.first_name,
+          last_name: appointment.last_name,
+          inc_book: appointment.inc_book,
+          out_book: appointment.out_book,
+          time: appointment.time,
+          date: appointment.date,
+          status: appointmentStatus[1],
+          location: appointment.location
+        }
+
+        const returnedAppointment = await fetch(`http://localhost:4001/appointments/${appointment._id}`, {
+          method: "PUT",
+          headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json", 'Authorization': `Bearer ${user.token}`
+          },
+          body: JSON.stringify(updatedAppointment)
+        })
+
+        updateBooks()
+        updateAppointments()
     }
-
-    const returnedOutBook = await fetch(`http://localhost:4001/books/${appointment.out_book._id}`, {
-      method: "PUT",
-      headers: {
-        Accept: "application/json",
-        "Content-Type": "application/json", 'Authorization': `Bearer ${user.token}`,
-      },
-      body: JSON.stringify(updatedOutBook)
-    })
-
-    const updatedAppointment = {
-      first_name: appointment.first_name,
-      last_name: appointment.last_name,
-      inc_book: appointment.inc_book,
-      out_book: appointment.out_book,
-      time: appointment.time,
-      date: appointment.date,
-      status: appointmentStatus[1],
-      location: appointment.location
-    }
-
-    const returnedAppointment = await fetch(`http://localhost:4001/appointments/${appointment._id}`, {
-      method: "PUT",
-      headers: {
-        Accept: "application/json",
-        "Content-Type": "application/json", 'Authorization': `Bearer ${user.token}`
-      },
-      body: JSON.stringify(updatedAppointment)
-    })
-
-    updateBooks()
-    updateAppointments()
-
   }
 
   const denyBooks = async (appointment) => {
-
-    // let inc_book = books.find(book => book._id === appointment.inc_book._id)
-    // let out_book = books.find(book => book._id === appointment.out_book._id)
-    // let thisAppointment = appointments.find(appointment => appointment._id === appointment._id)
-    // const user = JSON.parse(sessionStorage.getItem('user'))
 
     const updatedIncBook = {
       title: appointment.inc_book.title,
@@ -342,6 +344,13 @@ const App = () => {
       body: JSON.stringify(updatedIncBook)
     })
 
+    if (returnedIncBook.status === 401 ){
+      console.log('Error: Token Expired')
+      logout()
+      nav('/login')
+
+      return
+    }
 
     const updatedOutBook = {
       title: appointment.out_book.title,
